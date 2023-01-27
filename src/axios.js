c181b5d69794ac40428da91b84d796328137c27e
Jestinxavier <75082931+Jestinxavier@users.noreply.github.com>
2023-01-27 18:30:33 +0530

    Delete src directory

--- src/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-import { baseURL } from './const/const';
-const instance = axios.create({
-    baseURL: baseURL,
-  });
-  export default instance
--- src/const/const.js
+++
@@ -1,3 +0,0 @@
-export const baseURL ='https://api.themoviedb.org/3';
-export const API_KEY = '82045d36dc72688ed599ec10da099753';
-export const imageUrl = 'https://image.tmdb.org/t/p/original';
